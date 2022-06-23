c4c281d315ea35fda1619e07fab1848e92bd130b
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-06-23 17:01:26 -0300

    test: Product __repr__

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    sut = Product(
+        1,
+        "Farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "666172696E6861",
+        "ao abrigo de luz",
+    )
+
+    report = sut.__repr__()
+
+    assert report == (
+        "O produto Farinha"
+        " fabricado em 01-05-2021"
+        " por Farinini com validade"
+        " até 02-06-2023"
+        " precisa ser armazenado ao abrigo de luz."
+    )
