47048fd7a46e8c4eb27284f1b80a44d8470f24ea
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-06-25 00:56:15 -0300

    fix: linter inventory_refactor

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -5,8 +5,8 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 class InventoryRefactor(Iterable):
     def __init__(self, importer):
         self.importer = importer()
-        self.data = ''
-        self.report_type = ''
+        self.data = ""
+        self.report_type = ""
 
     def __iter__(self):
         return InventoryIterator(self.importer, self.data)
@@ -14,4 +14,3 @@ class InventoryRefactor(Iterable):
     def import_data(self, path, report_type):
         self.data += path
         self.report_type += report_type
-
