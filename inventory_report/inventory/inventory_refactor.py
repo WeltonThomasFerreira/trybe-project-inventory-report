cc7c50a26bbdece96a4d76bfeefe2b118f4cfe10
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-07-01 23:19:27 -0300

    feat(req11): implements main

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,6 +1,9 @@
 from typing import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
 
 class InventoryRefactor(Iterable):
     data = []
@@ -14,3 +17,9 @@ class InventoryRefactor(Iterable):
 
     def import_data(self, path, report_type):
         self.data += self.importer.import_data(path)
+
+    def generate(self, report_type):
+        if report_type == "simples":
+            return SimpleReport.generate(self.data)
+        elif report_type == "completo":
+            return CompleteReport.generate(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    inventoryRefactor = None
+
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    if ".csv" in sys.argv[1]:
+        inventoryRefactor = InventoryRefactor(CsvImporter)
+    if ".json" in sys.argv[1]:
+        inventoryRefactor = InventoryRefactor(JsonImporter)
+    if ".xml" in sys.argv[1]:
+        inventoryRefactor = InventoryRefactor(XmlImporter)
+
+    inventoryRefactor.import_data(sys.argv[1], sys.argv[2])
+    report = inventoryRefactor.generate(sys.argv[2])
+    print(report, end="")
